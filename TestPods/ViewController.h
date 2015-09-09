3d1a1ea2ec654c1e386efe8d0f215affa0cda8cb
zhzenghui <zhzenghui@gmail.com>
2015-09-09 16:29:17 +0800

    总算没错了版

--- TestPods/ViewController.h
+++ TestPods/ViewController.h
@@ -1,184 +1,21 @@
-@@ -1,183 +0,0 @@
 //
-//  ViewController.m
+//  ViewController.h
 //  TestPods
 //
 //  Created by xy on 15/8/25.
 //  Copyright (c) 2015年 XY. All rights reserved.
 //
 
-#import "ViewController.h"
-#import "AFNetworking.h"
-
-#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+#import "ReformerProtocol.h"
 
 
+@interface ViewController : UIViewController
 
 
-
-static NSString *DownloadURLString = @"http://m2.pc6.com/mac/OmniGrafflePro.dmg";
-
-@interface ViewController () < NSURLSessionDelegate, NSURLSessionTaskDelegate, NSURLSessionDownloadDelegate,UIDocumentInteractionControllerDelegate>
-
-@end
-
-@implementation ViewController
-
-
-
-
--(void)presentNotification{
-    
-    if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]){
-        
-        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound categories:nil]];
-        
-    }
-    
-    
-    
-    
-    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
-    localNotification.alertBody = @"Download Complete!";
-    localNotification.alertAction = @"Background Transfer Download!";
-    
-    //On sound
-    localNotification.soundName = UILocalNotificationDefaultSoundName;
-    
-    //increase the badge number of application plus 1
-    localNotification.applicationIconBadgeNumber = [[UIApplication sharedApplication] applicationIconBadgeNumber] + 1;
-    
-    [[UIApplication sharedApplication] presentLocalNotificationNow:localNotification];
-}
-
-
-
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    
-    self.session = [self backgroundSession];
-    
-    self.progressView.progress = 0;
-    self.progressView.hidden = YES;
-    
-    
-    
-    
-    
-    //    [self start:nil];
-    
-    
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-
-#pragma mark -
-
-- (IBAction)start:(id)sender {
-    if (self.downloadTask) {
-        return;
-    }
-    
-    NSURL *downloadURL = [NSURL URLWithString:DownloadURLString];
-    NSURLRequest *request = [NSURLRequest requestWithURL:downloadURL];
-    self.downloadTask = [self.session downloadTaskWithRequest:request];
-    [self.downloadTask resume];
-    self.progressView.hidden = NO;
-}
-
-
-
-- (NSURLSession *)backgroundSession {
-    static NSURLSession *session = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration backgroundSessionConfiguration:@"com.example.apple-samplecode.SimpleBackgroundTransfer.BackgroundSession"];
-        session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
-    });
-    return session;
-}
-
-
-- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
-    
-    if (downloadTask == self.downloadTask) {
-        double progress = (double)totalBytesWritten / (double)totalBytesExpectedToWrite;
-        NSLog(@"DownloadTask: %@ progress: %lf", downloadTask, progress);
-        
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.progressView.progress = progress;
-        });
-    }
-}
-
-- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)downloadURL {
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    
-    NSArray *URLs = [fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
-    NSURL *documentsDirectory = [URLs objectAtIndex:0];
-    
-    NSURL *originalURL = [[downloadTask originalRequest] URL];
-    NSURL *destinationURL = [documentsDirectory URLByAppendingPathComponent:[originalURL lastPathComponent]];
-    NSError *errorCopy;
-    
-    // For the purposes of testing, remove any esisting file at the destination.
-    [fileManager removeItemAtURL:destinationURL error:NULL];
-    BOOL success = [fileManager copyItemAtURL:downloadURL toURL:destinationURL error:&errorCopy];
-    
-    if (success) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            //download finished - open the pdf
-            self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:destinationURL];
-            
-            // Configure Document Interaction Controller
-            [self.documentInteractionController setDelegate:self];
-            
-            // Preview PDF
-            [self.documentInteractionController presentPreviewAnimated:YES];
-            self.progressView.hidden = YES;
-        });
-    } else {
-        NSLog(@"Error during the copy: %@", [errorCopy localizedDescription]);
-    }
-}
-
-
-- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
-    
-    if (error == nil) {
-        NSLog(@"Task: %@ completed successfully", task);
-        [self presentNotification];
-        
-    } else {
-        NSLog(@"Task: %@ completed with error: %@", task,   error  );
-    }
-    
-    double progress = (double)task.countOfBytesReceived / (double)task.countOfBytesExpectedToReceive;
-    dispatch_async(dispatch_get_main_queue(), ^{
-        self.progressView.progress = progress;
-    });
-    
-    self.downloadTask = nil;
-}
-
-- (void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)session {
-    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-    if (appDelegate.backgroundSessionCompletionHandler) {
-        void (^completionHandler)() = appDelegate.backgroundSessionCompletionHandler;
-        appDelegate.backgroundSessionCompletionHandler = nil;
-        completionHandler();
-    }
-    
-    NSLog(@"All tasks are finished");
-}
-
+@property (nonatomic, strong) id<ReformerProtocol> XXXReformer;
+@property (nonatomic, strong) id<ReformerProtocol> YYYReformer;
 
 
 @end
+
